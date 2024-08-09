d9965ec1f6a2518a7c5f37c2d9f0487758d731d0
torkin <daniele.laprova@hotmail.it>
2024-08-09 17:52:30 +0200

    fixed import warning

--- dataminer/src/test/java/it/torkin/dataminer/ApachejitDaoTest.java
+++ dataminer/src/test/java/it/torkin/dataminer/ApachejitDaoTest.java
@@ -1,7 +1,6 @@
 package it.torkin.dataminer;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
