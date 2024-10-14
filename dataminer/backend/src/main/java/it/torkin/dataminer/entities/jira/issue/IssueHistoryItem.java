339a5cf46111a59c6b22e21fba2bdb3badce08a5
Daniele La Prova <daniele.laprova2@gmail.com>
2024-10-14 16:45:55 +0200

    fix #117

--- dataminer/backend/src/main/java/it/torkin/dataminer/entities/jira/issue/IssueHistoryItem.java
+++ dataminer/backend/src/main/java/it/torkin/dataminer/entities/jira/issue/IssueHistoryItem.java
@@ -11,10 +11,12 @@ public class IssueHistoryItem {
     private String field;
     private String fieldtype;
 
+    @Column(name = "from_", columnDefinition = "text")
     private String from;
     @Column(columnDefinition = "text")
     private String fromString;
 
+    @Column(name = "to_", columnDefinition = "text")
     private String to;
     @Column(columnDefinition = "text")
     private String toString;
