0e31f45a39798b288d6dc6ecff9f13ab19726088
Daniele La Prova <daniele.laprova2@gmail.com>
2025-02-12 16:29:54 +0100

    divide columns count into feature families

--- dataminer/backend/scripts/count-columns.py
+++ dataminer/backend/scripts/count-columns.py
@@ -3,16 +3,42 @@
 import csv
 import sys
 
+FEATURE_FAMILY_SEPARATOR = ":"
+FEATURE_VARIANT_SEPARATOR = "-"
+
+def has_family(feature):
+    return FEATURE_FAMILY_SEPARATOR in feature
+
 def count_columns_in_csv(file_path):
     """
     Counts the number of columns in the first row of a CSV file.
     """
     try:
         with open(file_path, mode='r', newline='', encoding='utf-8') as csv_file:
+            counts = {}
+            nlp4re_counts = {}
             reader = csv.reader(csv_file)
             # Get the first row to determine the number of columns
             header = next(reader, None)
-            return len(header) if header else 0
+            counts["all"] = len(header) if header else 0
+
+            # group by feature family
+            for feature in header:
+                if has_family(feature):
+                    family = feature.split(FEATURE_FAMILY_SEPARATOR)[0]
+                else:
+                    family = "Target"
+                if family not in counts:
+                    counts[family] = 0
+                counts[family] += 1
+
+                if "nlp4re" in feature:
+                    feature = feature.split(FEATURE_FAMILY_SEPARATOR)[1].split(FEATURE_VARIANT_SEPARATOR)[0]
+                    if feature not in nlp4re_counts:
+                        nlp4re_counts[feature] = 0
+                    nlp4re_counts[feature] += 1    
+            return counts, nlp4re_counts
+
     except FileNotFoundError:
         print(f"Error: The file '{file_path}' does not exist.")
         sys.exit(1)
@@ -26,5 +52,6 @@ if __name__ == "__main__":
         sys.exit(1)
     
     file_path = sys.argv[1]
-    column_count = count_columns_in_csv(file_path)
-    print(f"The CSV file has {column_count} columns.")
+    counts, nlp4re_counts = count_columns_in_csv(file_path)
+    print(f"counts: {counts}")
+    print(f"nlp4re_counts: {nlp4re_counts}")
